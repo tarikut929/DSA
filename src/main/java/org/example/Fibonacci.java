cc9d9aefcd3d6fab98c2ed7e95253363ae8fb0aa
tarikut929 <tarikuw929@gmail.com>
2024-10-21 15:15:11 -0400

    implemented Fibonacci numbers using recursion

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -3,6 +3,7 @@ package org.example;
 public class Fibonacci {
     int firstNum = 0;
     int secondNum = 1;
+    int count = 1;
     public void showFibonacci(){
         System.out.println(firstNum);
         System.out.println(secondNum);
@@ -12,5 +13,23 @@ public class Fibonacci {
             firstNum = temp;
             System.out.println(secondNum);
         }
+    }
+    public void showFibonacciRecrs(int firstNum, int secondNum){
+        if ( count < 21){
+            int fib = firstNum + secondNum;
+            System.out.println(fib);
+            firstNum = secondNum;
+            secondNum = fib;
+            count++;
+
+            showFibonacciRecrs(firstNum, secondNum);
+
+        }
+        System.out.println();
+    }
+    public void showFibonacciRec(){
+        System.out.println(firstNum);
+        System.out.println(secondNum);
+        showFibonacciRecrs(firstNum, secondNum);
     }
 }
